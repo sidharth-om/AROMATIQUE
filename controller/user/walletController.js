efceb47362dfc2b668466a9612d91bb66d1c096f
sidharth-om <sidharthothayamangalam@gmail.com>
2025-08-05 15:28:57 +0530

    Wallet pagination

--- controller/user/walletController.js
+++ controller/user/walletController.js
@@ -9,15 +9,24 @@ const walletController={
         try {
             const userId=req.session.user.userId
 
+            const currentPage=parseInt(req.query.page)||1
+            const perPage=5
+            const skip=(currentPage-1)*perPage
+
+
             const user=await User.findById(userId)
+            const cart=await Cart.findOne({userId})
 
-            const transaction= await WalletTransaction.find({userId})
+            const totalTransactions=await WalletTransaction.countDocuments({userId})
 
-             const cart=await Cart.findOne({userId})
+            const transaction= await WalletTransaction.find({userId}).sort({createdAt:-1}).skip(skip).limit(perPage)
+
+            const totalPages=Math.ceil(totalTransactions/perPage)
+            
             console.log("user::",user)
 
             // const balance=user.wallet.balance
-            res.render("user/wallet",{transaction,user,cart})
+            res.render("user/wallet",{transaction,user,cart,currentPage,totalPages,perPage,totalTransactions})
         } catch (error) {
             console.log(error.message)
         }
